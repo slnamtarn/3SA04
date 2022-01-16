0f180d09ddd1310e3236f77bd90c1a743653859c
slnamtarn <6210110566@psu.ac.th>
2022-01-16 22:30:56 +0700

    test http

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello My First Web Application"
+#@app.route('/')
+#def home():
+    #return "Hello My First Web Application"
 
-@app.route('/login')
+@app.route('/')
 def login():
-    return "render_tamplate("login.html")
+    return render_tamplate('login.html')
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000',debug=True)
