c86d9a05652934ae3a12c5d3314b782a26853f55
slnamtarn <6210110566@psu.ac.th>
2022-01-16 22:16:13 +0700

    test http

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello My First Web Application"
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000',debug=True)
+   app.run(debug = True)
